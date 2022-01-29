bf4955c223f06daafabfecaf3e6550cdbdcf267b
Shiva <shishy212002@gmail.com>
2022-01-29 10:30:29 +0530

    Initialized

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+//Many important widget of flutter
+// Widgets for app setup
+// 1. Material App/CupertinoApp
+// 2. Scaffold/CupertinoPageScaffold
+// Widgets for layout
+// 1. Container(have just one child)
+// 2. Row and Column(have more than one child)
+// * Flexible and Expanded : wrap around the child to define how much space it will take in column/rows
+// Content Container:
+// 1. Stack(on top of each other)
+// 2. Card(gives some padding and border )
+// Repeat Element
+// 1. Grid(multiple item setting next to each other)
+// 2. Listview
+// 3. ListTile
+//Content types:
+//1. Text
+//2. Image
+//3. Icons
+
+//User Input
+//1. TextField
+//2. Buttons
+//3. Gesture Detector/Inkwell
